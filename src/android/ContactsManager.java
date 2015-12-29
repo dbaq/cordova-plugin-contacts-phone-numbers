a8076833ab8c91f01457253b188c6ef9c4bb5f33
David Rivas <jrivas@callpalapp.net>
2015-12-29 13:19:51 -0500

    Testing multiple phones

--- src/android/ContactsManager.java
+++ src/android/ContactsManager.java
@@ -56,7 +56,7 @@ public class ContactsManager extends CordovaPlugin {
           Context context = this.cordova.getActivity().getApplicationContext();
 
           JSONArray group = (JSONArray) args.get(0);
-          Log.d("data plugin:" ,String.valueOf(group.get(0)));
+          Log.d("data plugin:" ,String.valueOf(group.length()));
 
           //or Context context=cordova.getActivity().getApplicationContext();
           final Intent intent = new Intent(Intent.ACTION_INSERT, Contacts.CONTENT_URI);
